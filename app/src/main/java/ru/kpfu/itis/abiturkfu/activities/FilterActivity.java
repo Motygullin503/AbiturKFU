0e95a8ce4d9b33eaab8977f8f39c85784ba21617
ILMAZ13 <ilmaz1309@gmail.com>
2018-04-24 02:07:14 +0300

    Finished filter layout

--- app/src/main/java/ru/kpfu/itis/abiturkfu/activities/FilterActivity.java
+++ app/src/main/java/ru/kpfu/itis/abiturkfu/activities/FilterActivity.java
@@ -1,9 +1,10 @@
 package ru.kpfu.itis.abiturkfu.activities;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,15 +12,17 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import ru.kpfu.itis.abiturkfu.R;
+import ru.kpfu.itis.abiturkfu.databinding.ActivityFilterBinding;
 
 public class FilterActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button showFiltered;
+    private ActivityFilterBinding r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_filter);
+        r = DataBindingUtil.setContentView(this, R.layout.activity_filter);
 
         Toolbar toolbar = findViewById(R.id.my_toolbar);
 
@@ -35,9 +38,51 @@ public class FilterActivity extends AppCompatActivity implements View.OnClickLis
         title.setText("Фильтры");
 
         showFiltered = findViewById(R.id.show_filtered);
-
-
         showFiltered.setOnClickListener(this);
+
+        initFormsGroup();
+        initTypesGroup();
+        initOfficesGroup();
+
+    }
+
+    private void initTypesGroup() {
+        r.button1Types.setOnClickListener(v -> {
+            r.button1Types.toggle();
+        });
+        r.button2Types.setOnClickListener(v -> {
+            r.button2Types.toggle();
+        });
+        r.button3Types.setOnClickListener(v -> {
+            r.button3Types.toggle();
+        });
+    }
+
+    private void initOfficesGroup() {
+        r.button1Offices.setOnClickListener(v -> {
+            r.button1Offices.toggle();
+        });
+        r.button2Offices.setOnClickListener(v -> {
+            r.button2Offices.toggle();
+        });
+        r.button3Offices.setOnClickListener(v -> {
+            r.button3Offices.toggle();
+        });
+        r.button4Offices.setOnClickListener(v -> {
+            r.button4Offices.toggle();
+        });
+    }
+
+    private void initFormsGroup() {
+        r.button1Forms.setOnClickListener(v -> {
+            r.button1Forms.toggle();
+        });
+        r.button2Forms.setOnClickListener(v -> {
+            r.button2Forms.toggle();
+        });
+        r.button3Forms.setOnClickListener(v -> {
+            r.button3Forms.toggle();
+        });
     }
 
 
