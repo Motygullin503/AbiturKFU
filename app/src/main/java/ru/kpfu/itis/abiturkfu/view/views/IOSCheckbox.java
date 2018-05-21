63b5177779c70467406403103a892dac8b03682c
Bulat Motygullin <bul3515@gmail.com>
2018-05-21 16:31:02 +0300

    Булат, я пытался

---
+++ app/src/main/java/ru/kpfu/itis/abiturkfu/view/views/IOSCheckbox.java
@@ -0,0 +1,12 @@
+package ru.kpfu.itis.abiturkfu.view.views;
+
+import android.content.Context;
+
+
+
+public class IOSCheckbox extends android.support.v7.widget.AppCompatCheckBox {
+
+    public IOSCheckbox(Context context) {
+        super(context);
+    }
+}
